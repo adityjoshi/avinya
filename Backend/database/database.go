1e4dd00819ae9d763604559f76f0b54ff089e495
Aditya Joshi <aditya03joshi@gmail.com>
2024-11-09 10:52:42 +0530

    go

--- Backend/database/database.go
+++ Backend/database/database.go
@@ -10,7 +10,7 @@ import (
 
 func InitDatabase() {
 	var err error
-	dsn := "host=localhost user=postgres password=aditya dbname=hospital port=5432"
+	dsn := "host=localhost user=postgres password=aditya dbname=hosp port=5432"
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
